e7fb70df17f3468c096d33ae77b9fce32e3ce41a
medstuds <68807616+medstuds@users.noreply.github.com>
2020-07-29 23:13:46 +0700

    Update conf.py

--- direktoriku/source/conf.py
+++ direktoriku/source/conf.py
@@ -88,6 +88,7 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_themes', ]
+master_doc = 'index'
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
